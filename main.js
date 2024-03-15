95c5dc2b6e2cfb677693e4351df5416c9cf96aa5
Usama Muzammil <160235187+UsamaIT786@users.noreply.github.com>
2024-03-15 18:15:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//Stores Names
+//Persons
+var Names = ["Usama", "Muzammil", "Bilal", "Arsalan", "Farhan"];
+console.log(Names);
+console.log(0);
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
